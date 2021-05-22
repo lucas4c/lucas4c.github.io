a1e1d6d9706952e9d877a2f108d66856041dd3e4
lucas4c <77551182+lucas4c@users.noreply.github.com>
2021-05-22 19:21:44 -0300

    Add files via upload

---
+++ public/data/data.js
@@ -0,0 +1,164 @@
+export default 
+    {
+        enUS : {
+            hello : "Hi, I'm",
+            name : "Luke",
+            tagline : "I'm a Design-Minded Software Engineer and CompSci Student living in Brazil.",
+            contact : "Get in touch",
+            email : "lucas4c@hotmail.com",
+            backgroundTitle : "Background",
+            backgroundText : ["Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus officia voluptatibus tenetur perspiciatis ratione magnam quia, earum placeat nostrum quasi numquam optio voluptatum incidunt, eos odio, mollitia libero sapiente magni totam dolorum eveniet aspernatur non sint! Minima placeat nam ipsa!",
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus officia voluptatibus tenetur perspiciatis ratione magnam quia, earum placeat nostrum quasi numquam optio voluptatum incidunt, eos odio, mollitia libero sapiente magni totam dolorum eveniet aspernatur non sint! Minima placeat nam ipsa!"],
+            skillsTitle : "Skills",
+                skills : [
+                {
+                    name : "Languages",
+                    content : ["JavaScript", "TypeScript", "HTML", "CSS", "SQL", "Python", "PHP", "C", "C++"]
+                },
+                {
+                    name : "Frameworks",
+                    content : ["Node", "React", "React Native", "Next.js"]
+                },
+                {
+                    name : "Tools",
+                    content : ["Git & Github", "MongoDB", "Chrome DevTools"]
+                },
+                {
+                    name : "Design",
+                    content : ["Photoshop", "Prototyping", "User Testing"]
+                }
+            ],
+            experienceTitle : "Experience & Education",
+            experience : [
+                {
+                    place : "Poslope Digital Solutions",
+                    time : "2020 - Present",
+                    position : "CEO"
+                },
+                {
+                    place : "Universidade Federal do Rio Grande do Sul",
+                    time : "2019 - Present",
+                    position : "ComputerScience Graduate Student"
+                },
+                {
+                    place : "QI: Technical School",
+                    time : "2016 - 2018",
+                    position : "I.T. Certificate"
+                }
+            ],
+            projectsTitle : "Featured Projects",
+            projects : [
+                {
+                    name : "Hall",
+                    description : "Music News Website",
+                    image : "./public/images/hall-macbook.png"
+                }
+
+            ],
+
+            socialMedia : [
+                { 
+                    link : "https://www.instagram.com/luke4c",
+                    image : "/public/images/colored-instagram.svg"
+                },
+                { 
+                    link : "https://twitter.com/lucas_4c",
+                    image : "/public/images/colored-twitter.svg"
+                },
+                { 
+                    link : "https://www.facebook.com/luke4c",
+                    image : "/public/images/colored-facebook.svg"
+                },
+                { 
+                    link : "https://github.com/lucas4c",
+                    image : "/public/images/colored-github.svg"
+                },
+                { 
+                    link : "https://www.linkedin.com/in/lucas-nascimento-5b546217a",
+                    image : "/public/images/colored-linkedin.svg"
+                },
+                
+            ]
+        },
+
+        ptBR : {
+            hello : "Olá, eu sou o",
+            name : "Lucas",
+            tagline : "Graduando em Ciência da Computação e programador focado em Design vivendo no Brasil.",
+            contact : "Entre em contato",
+            email : "lucas4c@hotmail.com",
+            backgroundTitle : "Background",
+            backgroundText : ["Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus officia voluptatibus tenetur perspiciatis ratione magnam quia, earum placeat nostrum quasi numquam optio voluptatum incidunt, eos odio, mollitia libero sapiente magni totam dolorum eveniet aspernatur non sint! Minima placeat nam ipsa!",
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus officia voluptatibus tenetur perspiciatis ratione magnam quia, earum placeat nostrum quasi numquam optio voluptatum incidunt, eos odio, mollitia libero sapiente magni totam dolorum eveniet aspernatur non sint! Minima placeat nam ipsa!"],
+            skillsTitle : "Habilidades",
+            skills : [
+                {
+                    name : "Linguagens",
+                    content : ["JavaScript", "TypeScript", "HTML", "CSS", "SQL", "Python", "PHP", "C", "C++"]
+                },
+                {
+                    name : "Frameworks",
+                    content : ["Node", "React", "React Native", "Next.js"]
+                },
+                {
+                    name : "Ferramentas",
+                    content : ["Git & Github", "MongoDB", "Chrome DevTools"]
+                },
+                {
+                    name : "Design",
+                    content : ["Photoshop", "Prototyping", "User Testing"]
+                }
+            ],
+            experienceTitle : "Experiência e Educação",
+            experience : [
+                {
+                    place : "Poslope Digital Solutions",
+                    time : "2020 - Presente",
+                    position : "CEO"
+                },
+                {
+                    place : "Universidade Federal do Rio Grande do Sul",
+                    time : "2019 - Presente",
+                    position : "Graduação em Ciência da Computação"
+                },
+                {
+                    place : "QI: Technical School",
+                    time : "2016 - 2018",
+                    position : "Certificado em Técnico em Informática"
+                }
+            ],
+            projectsTitle : "Projetos Apresentados",
+            projects : [
+                {
+                    name : "Hall",
+                    description : "Site de Notícias de Músicas",
+                    image : "./public/images/hall-macbook.png"
+                }
+
+            ],
+
+            socialMedia : [
+                { 
+                    link : "https://www.instagram.com/luke4c",
+                    image : "/public/images/colored-instagram.svg"
+                },
+                { 
+                    link : "https://twitter.com/lucas_4c",
+                    image : "/public/images/colored-twitter.svg"
+                },
+                { 
+                    link : "https://www.facebook.com/luke4c",
+                    image : "/public/images/colored-facebook.svg"
+                },
+                { 
+                    link : "https://github.com/lucas4c",
+                    image : "/public/images/colored-github.svg"
+                },
+                { 
+                    link : "https://www.linkedin.com/in/lucas-nascimento-5b546217a",
+                    image : "/public/images/colored-linkedin.svg"
+                },
+                
+            ]
+        }
+    }
