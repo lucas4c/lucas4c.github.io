d28f8d66b82056e1bd82b05bd8177d6bad6bcfd2
lucas4c <77551182+lucas4c@users.noreply.github.com>
2023-05-29 21:43:32 -0300

    Added Salao Silvia

---
+++ data.js
@@ -0,0 +1,183 @@
+export default 
+    {
+        enUS : {
+            hello : "Hi, I'm",
+            name : "Luke",
+            tagline : "I'm a Design-Minded Software Engineer and CompSci Student living in Brazil.",
+            contact : "Get in touch",
+            email : "lucas4c@hotmail.com",
+            backgroundTitle : "Background",
+            backgroundText : ["I'm currently graduating on Computer Science at Universidade Federal do Rio Grande do Sul in Porto Alegre, Brazil. Before that, I got an I.T. Certificate from QI: Technical School. I'm also working with a group of awesome people on building a Digital Solutions Startup called Poslope.",
+                                "As a Software Engineer, I enjoy bridging the gap between engineering and design - focusing on creating a product that joins scalabilty and efficiency, to an engaging and intuitive, pixel-perfect user experience.",
+                                "When I'm not in front of the computer, I'm probably cooking or playing guitar. Maybe at the gym or eating a pizza. Ironic, yeah, I know."],
+            skillsTitle : "Skills",
+                skills : [
+                {
+                    name : "Languages",
+                    content : ["JavaScript", "TypeScript", "HTML", "CSS", "SQL", "Python", "PHP", "C", "C++"]
+                },
+                {
+                    name : "Frameworks",
+                    content : ["Node", "React", "React Native", "Next.js"]
+                },
+                {
+                    name : "Tools",
+                    content : ["Git & Github", "MongoDB", "Chrome DevTools"]
+                },
+                {
+                    name : "Design",
+                    content : ["Photoshop", "Prototyping", "User Testing"]
+                }
+            ],
+            experienceTitle : "Experience & Education",
+            experience : [
+                {
+                    place : "Poslope Digital Solutions",
+                    time : "2020 - Present",
+                    position : "CEO"
+                },
+                {
+                    place : "Universidade Federal do Rio Grande do Sul",
+                    time : "2019 - Present",
+                    position : "ComputerScience Graduate Student"
+                },
+                {
+                    place : "QI: Technical School",
+                    time : "2016 - 2018",
+                    position : "I.T. Certificate"
+                }
+            ],
+            projectsTitle : "Featured Projects",
+            projects : [
+                {
+                    name : "Hall",
+                    description : "Music News Website",
+                    size: "tall",
+                    style: "simple",
+                    image : "./public/images/hall-macbook.png"
+                },
+                {
+                    name : "Salão Silvia Presentation",
+                    description : "Business Presentation Website",
+                    style: "simple",
+                    size: "tall",
+                    image : "./public/images/salaosilvia.png"
+                }
+
+            ],
+
+            socialMedia : [
+                { 
+                    link : "https://www.instagram.com/luke.drg",
+                    image : "/public/images/colored-instagram.svg"
+                },
+                { 
+                    link : "https://twitter.com/lukeLXIX",
+                    image : "/public/images/colored-twitter.svg"
+                },
+                { 
+                    link : "https://www.facebook.com/luke4c",
+                    image : "/public/images/colored-facebook.svg"
+                },
+                { 
+                    link : "https://github.com/lucas4c",
+                    image : "/public/images/colored-github.svg"
+                },
+                { 
+                    link : "https://www.linkedin.com/in/lucas-nascimento-5b546217a",
+                    image : "/public/images/colored-linkedin.svg"
+                },
+                
+            ]
+        },
+
+        ptBR : {
+            hello : "Olá, eu sou o",
+            name : "Lucas",
+            tagline : "Graduando em Ciência da Computação e programador focado em Design vivendo no Brasil.",
+            contact : "Entre em contato",
+            email : "lucas4c@hotmail.com",
+            backgroundTitle : "Background",
+            backgroundText : ["Atualmente estou cursando Ciência da Computação na Universidade Federal do Rio Grande do Sul, em Porto Alegre. Depois de formado em Tecnologia da Informação pela Escolas e Faculdades QI. Também planejando e desenvolvendo junto a um grupo de pessoas incríveis uma Startup em Soluções Digitais chamada Poslope.",
+                                "Como Engenheiro de Software, gosto de explorar a união entre engenharia e design - focando sempre em criar um produto que une escalabilidade e eficiência, à uma experiência intuitiva e atraente ao usuário.",
+                                "Quando não estou na frente do computador, eu provavelmente estou cozinhando ou tocando violão. Talvez na academia, ou comendo uma pizza... Sim, irônico, eu sei."],
+            skillsTitle : "Habilidades",
+            skills : [
+                {
+                    name : "Linguagens",
+                    content : ["JavaScript", "TypeScript", "HTML", "CSS", "SQL", "Python", "PHP", "C", "C++"]
+                },
+                {
+                    name : "Frameworks",
+                    content : ["Node", "React", "React Native", "Next.js"]
+                },
+                {
+                    name : "Ferramentas",
+                    content : ["Git & Github", "MongoDB", "Chrome DevTools"]
+                },
+                {
+                    name : "Design",
+                    content : ["Photoshop", "Prototyping", "User Testing"]
+                }
+            ],
+            experienceTitle : "Experiência e Educação",
+            experience : [
+                {
+                    place : "Poslope Digital Solutions",
+                    time : "2020 - Presente",
+                    position : "CEO"
+                },
+                {
+                    place : "Universidade Federal do Rio Grande do Sul",
+                    time : "2019 - Presente",
+                    position : "Graduação em Ciência da Computação"
+                },
+                {
+                    place : "QI: Technical School",
+                    time : "2016 - 2018",
+                    position : "Certificado em Técnico em Informática"
+                }
+            ],
+            projectsTitle : "Projetos Apresentados",
+            projects : [
+                {
+                    name : "Hall",
+                    description : "Site de Notícias de Músicas",
+                    style: "simple",
+                    size: "tall",
+                    image : "./public/images/hall-macbook.png"
+                },
+                {
+                    name : "Site de Apresentação Salão Silvia",
+                    description : "Site Institucional",
+                    style: "simple",
+                    size: "tall",
+                    image : "./public/images/salaosilvia.png"
+                }
+            ],
+
+            socialMedia : [
+                { 
+                    link : "https://www.instagram.com/luke.drg",
+                    image : "/public/images/colored-instagram.svg"
+                },
+                { 
+                    link : "https://twitter.com/lukeLXIX",
+                    image : "/public/images/colored-twitter.svg"
+                },
+                { 
+                    link : "https://www.facebook.com/luke4c",
+                    image : "/public/images/colored-facebook.svg"
+                },
+                { 
+                    link : "https://github.com/lucas4c",
+                    image : "/public/images/colored-github.svg"
+                },
+                { 
+                    link : "https://www.linkedin.com/in/lucas-nascimento-5b546217a",
+                    image : "/public/images/colored-linkedin.svg"
+                },
+                
+            ]
+        }
+    }
